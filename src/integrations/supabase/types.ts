e656567cc7de62a45c664294507a1a6ab204f528
RooftopMash <borulelo@gmail.com>
2026-09-02 00:05:44 +0200

    Add incidents types

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -295,6 +295,46 @@ export type Database = {
           },
         ]
       }
+      incidents: {
+        Row: {
+          brand_id: string | null
+          created_at: string
+          description: string | null
+          id: string
+          lat: number | null
+          lng: number | null
+          media_type: string | null
+          media_url: string | null
+          title: string
+          user_id: string
+        }
+        Insert: {
+          brand_id?: string | null
+          created_at?: string
+          description?: string | null
+          id?: string
+          lat?: number | null
+          lng?: number | null
+          media_type?: string | null
+          media_url?: string | null
+          title: string
+          user_id: string
+        }
+        Update: {
+          brand_id?: string | null
+          created_at?: string
+          description?: string | null
+          id?: string
+          lat?: number | null
+          lng?: number | null
+          media_type?: string | null
+          media_url?: string | null
+          title?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
+
       messages: {
         Row: {
           body: string
